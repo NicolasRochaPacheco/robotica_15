c1a69045ec0bb100abb01581f6bac0ba2cf0ee4b
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2019-02-03 20:30:39 -0500

    Code structure developed. Char check still to do

--- controller_15/src/punto1.py
+++ controller_15/src/punto1.py
@@ -13,6 +13,9 @@
 # Importamos la libreria de rospy
 import rospy
 
+# Importamos la libreria de pygame como interfaz de usuario.
+import pygame
+
 # Importamos los mensajes y servicios de PacMan
 from pacman.msg import actions
 from pacman.msg import pacmanPos
@@ -27,12 +30,19 @@ from pacman.srv import mapService
 def posCallback(data):
 	pass
 
+# Funcion que devuelve la tecla que esta siendo presionada. Sera
+# lanzada como un hilo.
+
+
 #----------------------------------------------------------------------
 #	VARIABLES / CONSTANTES
 #----------------------------------------------------------------------
 
 # Variable para almacenar el nombre del jugador
 nombre = 'grupo_15'
+
+# Variable para mantener el nodo corriendo
+running = True
 
 #----------------------------------------------------------------------
 #	LOGICA DEL NODO
@@ -43,6 +53,24 @@ rospy.init_node('grupo15_punto1', anonymous=True)
 pub = rospy.Publisher('pacmanActions0', actions, queue_size=10)
 rospy.Subscriber('pacmanCoord0', pacmanPos, posCallback)
 
+# Inicializamos pygame y una ventana
+pygame.init()
+(width, height) = 	(300, 200)
+screen = pygame.display.set_mode((width, height))
+screen.fill((0, 0, 255))
+pygame.display.set_caption("Grupo 15: Punto 1")
+pygame.display.flip()
+
+# Loop infinito para mantener el nodo mientras sea necesario.
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			quit()
+
+
+
+
 # Enviamos la solicitud para iniciar el juego
 mapRequestClient = rospy.ServiceProxy('pacman_world', mapService)
 mapa = mapRequestClient(nombre)
@@ -50,8 +78,8 @@ mapa = mapRequestClient(nombre)
 # Enviamos el comando para controlar a Pacman
 rate = rospy.Rate(10)
 msg = actions()
+num = 0
 while not rospy.is_shutdown():
-	msg.action = 3
+	msg.action = num
 	pub.publish(msg.action)
 	rate.sleep()
-
